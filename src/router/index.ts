05e7ede22f57fed3ee7ae9a74d63612a1e54c8ec
LocalGit <btaworld@outlook.com>
2025-03-05 21:58:55 +0800

    重构404页面，替换为新的NotFoundView组件，并更新路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,7 +19,7 @@ const router = createRouter({
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
-      component: () => import('../views/NotFound.vue'),
+      component: NotFound,
     }
   ],
 })
